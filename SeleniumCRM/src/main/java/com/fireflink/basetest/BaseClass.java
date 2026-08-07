f38a5cd5a2693e5606bfebdf2521dbb92bcdeee9
Poornima8123 <poornima8123@gmail.com>
2026-08-07 10:37:47 +0530

    Complete Project is commited

--- SeleniumCRM/src/main/java/com/fireflink/basetest/BaseClass.java
+++ SeleniumCRM/src/main/java/com/fireflink/basetest/BaseClass.java
@@ -1,6 +1,9 @@
 package com.fireflink.basetest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
@@ -12,6 +15,7 @@ public class BaseClass {
 	@BeforeSuite
 	public void configBS()
 	{
+		WebDriver driver = new ChromeDriver();
 		ExtentSparkReporter spark = new ExtentSparkReporter("./AdvanceReport/report.html");
 		spark.config().setDocumentTitle("Advance Report Title");
 		spark.config().setReportName("CRM report");
@@ -22,6 +26,14 @@ public class BaseClass {
 		report = new ExtentReports();
 		report.attachReporter(spark);
 		report.setSystemInfo("BROWSER", "CHROME");
+		
+		
+	}
+	
+	@Test
+	public void test10() {
+		System.out.println("created");
+		System.out.println("created1");
 	}
 
 }
---
+++ SeleniumCRM/src/test/java/com/fireflink/practicetest/InvoiceTest.java
@@ -0,0 +1,22 @@
+package com.fireflink.practicetest;
+
+import java.sql.Driver;
+
+import org.testng.annotations.Test;
+
+import com.fireflink.basetest.BaseClass;
+
+public class InvoiceTest extends BaseClass {
+	
+	@Test
+	public void createInvoiceTest() {
+		System.out.println("execute create invoice test");
+		
+		
+		}
+	public void  createInvoicWithContactTest() {
+		System.out.println("execute createInvoicewithContactTest");
+	}
+	
+
+}
